e15808acfe1c3fdeef3c193ce6af8f310a598b1f
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-08-11 10:53:55 -0300

    feat(header): add link animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,73 @@
+"use client"
+
 import Link from "next/link"
-import Image from "next/image"
 
 import styles from "./Header.module.sass"
 import LogoItem from "../LogoItem/LogoItem"
 
+import { LuClapperboard } from "react-icons/lu"
+import { LuTv } from "react-icons/lu"
+import { ImFire } from "react-icons/im"
+import { FaRegBookmark } from "react-icons/fa"
+import { useEffect, useState } from "react"
+
 function Header() {
+    const [scrolled, setScrolled] = useState(false)
+
+    function handleScroll() {
+        setScrolled(window.scrollY > 0)
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
+
+        return () => window.removeEventListener("scroll", handleScroll)
+    }, [])
+
     return (
-        <header className={styles.header}>
+        <header className={`${styles.header} ${scrolled ? styles.scrolled : ""}`}>
             <nav className={styles.nav}>
                 <ul className={styles.nav_list}>
                     <li className={styles.nav_item}>
-                        <Link href="/films">Filmes</Link>
+                        <Link href="/movies">
+                            <span>Filmes</span>
+
+                            <span>
+                                <LuClapperboard />
+                            </span>
+                        </Link>
                     </li>
 
                     <li className={styles.nav_item}>
-                        <Link href="/series">Séries</Link>
+                        <Link href="/series">
+                            <span>Séries</span>
+
+                            <span>
+                                <LuTv />
+                            </span>
+                        </Link>
                     </li>
 
                     <LogoItem />
 
                     <li className={styles.nav_item}>
-                        <Link href="/popular">Populares</Link>
+                        <Link href="/popular">
+                            <span>Populares</span>
+
+                            <span>
+                                <ImFire />
+                            </span>
+                        </Link>
                     </li>
 
                     <li className={styles.nav_item}>
-                        <Link href="/watchlist">Watchlist</Link>
+                        <Link href="/watchlist">
+                            <span>Watchlist</span>
+
+                            <span>
+                                <FaRegBookmark />
+                            </span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
--- src/components/LogoItem/LogoItem.tsx
+++ src/components/LogoItem/LogoItem.tsx
@@ -5,20 +5,20 @@ import styles from "./LogoItem.module.sass"
 
 function LogoItem() {
     return (
-        <li className={styles.logo_item}>
+        <li>
             <Link href="/" className={styles.logo_link}>
                 <Image src="/logo_v2.png" width={50} height={50} alt="Logo do site" className={styles.logo} />
 
-                <div>
-                    <span>I</span>
-                    <span>N</span>
-                    <span>E</span>
-                    <span>M</span>
-                    <span>I</span>
-                    <span>D</span>
-                    <span>I</span>
-                    <span>A</span>
-                </div>
+                    <div>
+                        <span>I</span>
+                        <span>N</span>
+                        <span>E</span>
+                        <span>M</span>
+                        <span>I</span>
+                        <span>D</span>
+                        <span>I</span>
+                        <span>A</span>
+                    </div>
             </Link>
         </li>
     )
