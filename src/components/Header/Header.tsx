f597026d318f5ca479a5c44d93107487dd126926
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-09-11 14:37:26 -0300

    feat(header): add mobile version header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,7 @@ import styles from "./Header.module.sass"
 
 function Header() {
   const [scrolled, setScrolled] = useState(false)
+  const [show, setShow] = useState(false)
   const pathName = usePathname()
 
   const handleScroll = useCallback(() => {
@@ -27,8 +28,24 @@ function Header() {
     return () => window.removeEventListener("scroll", handleScroll)
   }, [handleScroll])
 
+  function handleMenu() {
+    setShow((prev) => !prev)
+  }
+
   return (
-    <header className={`${styles.header} ${scrolled ? styles.scrolled : ""}`}>
+    <header
+      className={`${styles.header} ${scrolled ? styles.scrolled : ""} ${show ? styles.show : ""}`}
+    >
+      <button
+        type="button"
+        className={`${styles.menu_btn} ${show ? styles.transform : ""}`}
+        onClick={handleMenu}
+      >
+        <div className={styles.menu_bar}></div>
+        <div className={styles.menu_bar}></div>
+        <div className={styles.menu_bar}></div>
+      </button>
+
       <nav className={styles.nav}>
         <ul className={styles.nav_list}>
           <li className={styles.nav_item}>
@@ -63,7 +80,7 @@ function Header() {
             </Link>
           </li>
 
-          <LogoItem />
+          <LogoItem pathName={pathName} />
 
           <li className={styles.nav_item}>
             <Link
