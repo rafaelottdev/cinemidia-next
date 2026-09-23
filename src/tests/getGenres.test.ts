a7f330153a2b0feb21550ddb531f2e1f389d9f17
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-09-23 14:46:14 -0300

    chore: remove click out search feature

--- src/tests/getGenres.test.ts
+++ src/tests/getGenres.test.ts
@@ -1,22 +1,20 @@
-import { describe, expect, it } from "vitest"
+import { expect, it } from "vitest"
 import getGenres from "@/lib/getGenres"
 import type { Genres } from "@/types/genres"
 
-describe("should return the movie genres", () => {
-  it("", async () => {
-    const genreList: Genres[] = [
-      {
-        id: 1,
-        name: "genre 1",
-      },
-      {
-        id: 2,
-        name: "genre 2",
-      },
-    ]
+it("should return the movie genres", async () => {
+  const genreList: Genres[] = [
+    {
+      id: 1,
+      name: "genre 1",
+    },
+    {
+      id: 2,
+      name: "genre 2",
+    },
+  ]
 
-    const genres = await getGenres()
+  const genres = await getGenres()
 
-    expect(genres).toEqual(genreList)
-  })
+  expect(genres).toEqual(genreList)
 })
